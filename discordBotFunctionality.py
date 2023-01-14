8922db863b95fa9f7a277afbbb729f7b08e5c2a1
DiegoERojas <diegoerojas16@gmail.com>
2023-01-14 12:57:46 -0600

    Added on_message event.

--- discordBotFunctionality.py
+++ discordBotFunctionality.py
@@ -13,25 +13,53 @@ bot = commands.Bot(command_prefix='.', intents=intents)
 # the lowercase input is equal to one of the keys, execute that key value pair. Otherwise, say that command doesn't exist
 
 def run_Discord_Bot():
+    chainMessages = 10
+    messageList = [""]
+    listOfWords = []
+
     @bot.event
     async def on_ready():
         print(f"{bot.user} is ready.")
 
+    @bot.event
+    async def on_message(message):
+        # Bot will ignore any messages sent by itself
+        if message.author == bot.user:
+            return
+        # If a user's message is a command for Markov or a link, then ignore it
+        if not message.content.startswith(".") and not message.content.startswith("http"):
+            print(f"Message contents: {message.content}")
+            messageList.insert(0, message.content.split())
+            print(f"messageList: {messageList}")
+        # Obtaining each individual work within the user message and storing in a list
+        for word in messageList[0]:
+            listOfWords.append(word)
+        print(f"listOfWords: {listOfWords}")
+        await bot.process_commands(message)
+
     @bot.command()
     async def markov(ctx):
-        await ctx.send("`I have awoken from my slumber.`")
+        await ctx.send("`Forcing a markov chain...`")
+
+    bot.remove_command("help")
 
     @bot.command()
-    async def elp(ctx):
-        await ctx.send("```Here is the list of things I can do: \n1) .markov\n2) .elp\n3) .WAKEUP\n4) .credits```")
+    async def help(ctx):
+        await ctx.send("```Here is the list of things I can do: \n1) .markov\n2) .help\n3) .description\n4) .credits\n5) .source```")
 
     @bot.command()
-    async def WAKEUP(ctx):
-        await ctx.send(f"`Hey {ctx.author}, FRICK YOU. Now I'm just going to stop running out of spite.`")
+    async def description(ctx):
+        await ctx.send(f"```Hello, I am a bot that relies on text from users and composes a markov chain.\n"
+                       f"This process will occur every {chainMessages} messages or can be forced using the\n"
+                       f"\'.markov\' command```")
 
     @bot.command()
     async def credits(ctx):
-        await ctx.send(f"`D E G O#5308 made me :) (Don't ask how long it took his dumbass)`")
+        await ctx.send(f"`D E G O#5308 made me :)`")
+
+    @bot.command()
+    async def source(ctx):
+        await ctx.send(f"`Repo: https://github.com/DiegoERojas/MarkovDiscordBot`")
 
     bot.run(creds.botTOKEN)
 

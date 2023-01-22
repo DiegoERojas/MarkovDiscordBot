04f37c2d452bad9d8e38bb06dcb8214bd63788d6
DiegoERojas <diegoerojas16@gmail.com>
2023-01-22 00:04:36 -0600

    used get_context to automatically run .markov

--- MarkovTextComposer.py
+++ MarkovTextComposer.py
@@ -1,7 +1,7 @@
 import random
 
 class MarkovComposer:
-    def __init__(self, listOfWords):
+    def __init__(self, listOfWords, lengthOfChain):
         self.listOfWords = listOfWords
         """
         A dictionary to hold all adjacent words with that word. Every pair of words will have their own key 
@@ -11,7 +11,8 @@ class MarkovComposer:
         self.adjacentWords = {}
         self.numberOfValues = 0
         self.output = ""
-        self.lengthOfChain = 0
+        self.lengthOfChain = lengthOfChain
+        self.CHAINCONSTANT = lengthOfChain
 
     def setLengthOfChain(self, length):
         self.lengthOfChain = length
@@ -31,12 +32,12 @@ class MarkovComposer:
                 self.adjacentWords[currentKey] += 1
                 continue
             self.adjacentWords[currentKey] = 1
+        return self.adjacentWords
 
     def markovOutput(self):
         # Storing each the dict. keys and values in their own list
         keyList = []
         valueList = []
-        finalOutput = ""
         for key in self.adjacentWords.keys():
             keyList.append(key)
         for value in self.adjacentWords.values():
@@ -44,11 +45,16 @@ class MarkovComposer:
         resultingString = random.choices(keyList, weights=valueList, k=self.numberOfValues)
         print(f"keyList: {keyList}\nvalueList: {valueList}")
         self.setOutput(resultingString)
+        return self.getOutput()
 
     def unpackingResult(self):
         finalPhrase = ""
-        for pair in self.output:
-            pairOfWords = " ".join(pair)
+        """
+        Loop amount is determined on how many pairs there is divided by the 
+        length of the chain set in discordBotFunctionality
+        """
+        for number in range(self.numberOfValues // self.CHAINCONSTANT):
+            pairOfWords = " ".join(self.getOutput()[number])
             finalPhrase = finalPhrase + " " + pairOfWords
         return finalPhrase
 
